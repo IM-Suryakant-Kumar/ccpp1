6154c7c5b169779a78d2fde1bbc9feec04061013
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-04-12 14:49:19 +0530

    refactor: Updated verify page and VerifyAction component

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -1,3 +1,5 @@
+import VerifyAction from "~/components/VerifyAction";
+
 const Verify = () => {
   return (
     <div className="mx-auto mt-8 w-[90%] max-w-[25rem] rounded-2xl border-2 border-[#c1c1c1] px-8 py-8 text-center">
@@ -7,44 +9,7 @@ const Verify = () => {
         Enter the 8 digit code you have received on swa***@gmail.com
       </p>
       {/* input boxex */}
-      <div className="my-4 flex flex-wrap justify-between">
-        <p className="font-semi-bold w-full text-left text-xs">Code</p>
-        <input
-          className="h-7 w-7 rounded-sm border border-[#c1c1c1] md:h-8 md:w-8"
-          type="number"
-        />
-        <input
-          className="h-7 w-7 rounded-sm border border-[#c1c1c1] md:h-8 md:w-8"
-          type="number"
-        />
-        <input
-          className="h-7 w-7 rounded-sm border border-[#c1c1c1] md:h-8 md:w-8"
-          type="number"
-        />
-        <input
-          className="h-7 w-7 rounded-sm border border-[#c1c1c1] md:h-8 md:w-8"
-          type="number"
-        />
-        <input
-          className="h-7 w-7 rounded-sm border border-[#c1c1c1] md:h-8 md:w-8"
-          type="number"
-        />
-        <input
-          className="h-7 w-7 rounded-sm border border-[#c1c1c1] md:h-8 md:w-8"
-          type="number"
-        />
-        <input
-          className="h-7 w-7 rounded-sm border border-[#c1c1c1] md:h-8 md:w-8"
-          type="number"
-        />
-        <input
-          className="h-7 w-7 rounded-sm border border-[#c1c1c1] md:h-8 md:w-8"
-          type="number"
-        />
-      </div>
-      <button className="mb-4 block h-8 w-full rounded-md bg-black text-xs text-white md:h-10 uppercase">
-        Verify
-      </button>
+      <VerifyAction />
     </div>
   );
 };
