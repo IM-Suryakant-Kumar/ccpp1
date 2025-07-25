b903a08fdf1f2d23cfeeedbec1bc70263635c32f
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2025-07-25 18:02:10 +0530

    feat: Added Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,39 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+    <main className="mx-auto flex min-h-screen max-w-7xl flex-col items-center justify-center gap-6 p-6 text-center md:flex-row">
+      <section className="flex flex-col items-center justify-center md:flex-1">
+        <Image
+          className="border-logo rounded-full border-4 p-0.5"
+          src="/logo.png"
+          alt="logo"
+          width={50}
+          height={50}
+        />
+        <h1 className="text-logo font-cinzel mt-2 text-2xl font-semibold">
+          CCPP1
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+        <p className="max-w-xs text-gray-600">
+          Connect and share your thought and snaps with friend and community
+        </p>
+        <Link
+          href="feed"
+          className="bg-logo text-primary font-cinzel mt-6 rounded-full px-6 py-1"
+        >
+          Get Started
+        </Link>
+      </section>
+      <section className="flex items-center justify-center md:flex-1">
+        <Image
+          className="w-full max-w-44 object-contain"
+          src="/hero.svg"
+          alt="hero image"
+          width={100}
+          height={100}
+        />
+      </section>
     </main>
   );
 }
